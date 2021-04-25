17e23cff713732a97df2072444165e1e51c66902
Craig Ceremuga <c.ceremuga@gmail.com>
2021-04-25 14:11:30 -0400

    Removed text

--- main.go
+++ main.go
@@ -68,43 +68,6 @@ var (
 	wonb     = flag.Bool("whiteonblack", true, "white text on a black background")
 )
 
-var text = []string{
-	"’Twas brillig, and the slithy toves",
-	"Did gyre and gimble in the wabe;",
-	"All mimsy were the borogoves,",
-	"And the mome raths outgrabe.",
-	"",
-	"“Beware the Jabberwock, my son!",
-	"The jaws that bite, the claws that catch!",
-	"Beware the Jubjub bird, and shun",
-	"The frumious Bandersnatch!”",
-	"",
-	"He took his vorpal sword in hand:",
-	"Long time the manxome foe he sought—",
-	"So rested he by the Tumtum tree,",
-	"And stood awhile in thought.",
-	"",
-	"And as in uffish thought he stood,",
-	"The Jabberwock, with eyes of flame,",
-	"Came whiffling through the tulgey wood,",
-	"And burbled as it came!",
-	"",
-	"One, two! One, two! and through and through",
-	"The vorpal blade went snicker-snack!",
-	"He left it dead, and with its head",
-	"He went galumphing back.",
-	"",
-	"“And hast thou slain the Jabberwock?",
-	"Come to my arms, my beamish boy!",
-	"O frabjous day! Callooh! Callay!”",
-	"He chortled in his joy.",
-	"",
-	"’Twas brillig, and the slithy toves",
-	"Did gyre and gimble in the wabe;",
-	"All mimsy were the borogoves,",
-	"And the mome raths outgrabe.",
-}
-
 func printForecastToImage(forecast []DailyForecast) error {
 	for _, day := range forecast {
 		fmt.Printf("%-20s %.0f / %.0f\n", day.Name, day.HighTemp, day.LowTemp)
@@ -150,6 +113,8 @@ func printForecastToImage(forecast []DailyForecast) error {
 		rgba.Set(10, 10+i, ruler)
 		rgba.Set(10+i, 10, ruler)
 	}
+
+	text := []string{"this", "is", "a", "test"}
 
 	// Draw the text.
 	pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
